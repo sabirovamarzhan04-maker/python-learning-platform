d35a417f0f29da5a83ce50aabde1a6e6ae76cf80
Ahmediya Olzhas <ahmediya04@gmail.com>
2026-05-20 18:20:32 +0500

    fix database url

--- дипломка/main.py
+++ дипломка/main.py
@@ -27,6 +27,9 @@ client = OpenAI(
 # -----------------------------------------
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./users.db")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 else:
